0a6cc8b5ea2183c9c887593ecf92ac06023023a2
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-01 15:16:15 +0600

    the change add nex text

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// // let dw = text => document.write(text + "<br>")
+
+// let arr = ["I", "go", "home"];
+
+// delete arr[1]; // удалить "go"
+
+// // alert( arr[1] ); // undefined
+
+// // теперь arr = ["I",  , "home"];
+// alert( arr.length ); // 3
+["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
